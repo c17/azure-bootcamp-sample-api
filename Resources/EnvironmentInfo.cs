8f9d26f95745888c313ff6b02a03e6efcb430b1b
gcurb <gcurb@outlook.fr>
2020-11-27 20:30:29 +0100

    init

---
+++ Resources/EnvironmentInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SampleApi.Resources
+{
+    public class EnvironmentInfo
+    {
+        public EnvironmentInfo(string machineName, string hostName, IEnumerable<string> ipAddresses, DateTimeOffset currentDate)
+        {
+            this.MachineName = machineName;
+            this.HostName = hostName;
+            this.IpAddresses = ipAddresses;
+            this.CurrentDate = currentDate;
+        }
+
+        public string MachineName { get; }
+        public string HostName { get; }
+        public IEnumerable<string> IpAddresses { get; }
+        public DateTimeOffset CurrentDate { get; }
+    }
+}
